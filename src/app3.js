005bec4a021a4ee0828bdf26d605048661718142
shawnjonchan <xiaojiechen86@gmail.com>
2020-08-01 18:05:00 +0800

    init

---
+++ src/app3.js
@@ -0,0 +1,24 @@
+import $ from "jquery";
+import "./app3.css";
+
+const $square = $("#app3 .square");
+const localKey = "app3.active";
+// yes no undefined
+const active = localStorage.getItem(localKey) === "yes";
+
+// if (active) {
+//   $square.addClass("active");
+// } else {
+//   $square.removeClass("active");
+// }
+$square.toggleClass("active", active);
+
+$square.on("click", () => {
+  if ($square.hasClass("active")) {
+    $square.removeClass("active");
+    localStorage.setItem(localKey, "no");
+  } else {
+    $square.addClass("active");
+    localStorage.setItem("app3.active", "yes");
+  }
+});
